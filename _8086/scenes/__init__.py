fbade2976429711646df76899ea7ce053df09108
mental <m3nta1@yahoo.com>
2018-12-23 01:31:09 +0000

    bugfix and fully implement Textbox

---
+++ _8086/scenes/__init__.py
@@ -0,0 +1,4 @@
+from .textbox import Textbox
+
+# def __getattribute__(name):
+# 	raise AttributeError('Module attribute access disallowed.')
--- _8086/scenes/textbox.py
+++ _8086/scenes/textbox.py
@@ -1,36 +1,42 @@
 import array
 
+import pygame
+
+NEWLINE = 13
+
+
 class Textbox:
-    def __init__(self, window, x, y width, height):
+    def __init__(self, window, x, y, width, height):
         self.screen = window.screen
 
         self.width = width
         self.height = height
         self.buffer = array.array('H')
 
-        self.text = lambda s, x, y, c: return window.text(string, (x, y), c or self.color, self.size)
+        self.color = (0, 255, 0)
+        self.bg = (0, 0, 0)
+        self.size = 20
+
+        self.text = lambda s, x, y, c=None: window.text(s, (x, y), c or self.color, self.size)
 
     @classmethod
     def from_window(cls, window):
         return cls(window, 0, 0, window.width, window.height)
 
-    def write(self):
-        pass
+    def write(self, *data):
+        self.buffer.extend(array.array('H', (key for key in data)))
 
-    def writeln(self, *args, **kwargs):
-        self.write(*args, '\n', **kwargs)
+    def writeln(self, *args):
+        self.write(*args, '\n')
 
     def update(self):
         self.screen.fill(self.bg)
 
-        buffer = self.buffer
-        sp = s = y = 0
+        y = 0
+        buffer = self.buffer.tobytes()[:-1].split(b'\x0d')
 
-        slice = buffer[s:]
-
-        while slice.count(NEWLINE):
-            s += self.text(buffer[s:slice.index(NEWLINE)].tostring(), 0, y, self.color)
-            slice = buffer[s:]
+        for line in buffer:
+            self.text(list(b for b in line if b), 0, y * self.size)
             y += 1
 
         pygame.display.update(self.screen.get_rect())
