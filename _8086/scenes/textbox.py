7a7395754cc258d7d3a2f714620d6afddb5516df
mental <m3nta1@yahoo.com>
2018-12-24 02:12:44 +0000

    bugfix write, writeln and update. Add drain and writestr

--- _8086/scenes/textbox.py
+++ _8086/scenes/textbox.py
@@ -6,37 +6,49 @@ NEWLINE = 13
 
 
 class Textbox:
-    def __init__(self, window, x, y, width, height):
+    def __init__(self, window, x, y, width, height, size=20, bg=(0, 0, 0), fg=(255, 255, 255)):
         self.screen = window.screen
 
         self.width = width
         self.height = height
         self.buffer = array.array('H')
 
-        self.color = (0, 255, 0)
-        self.bg = (0, 0, 0)
-        self.size = 20
+        self.bg, self.fg = bg, fg
+        self.size = size
 
-        self.text = lambda s, x, y, c=None: window.text(s, (x, y), c or self.color, self.size)
+        self.text = lambda s, x, y, c=None: window.text(s, (x, y), c or fg, size)
 
     @classmethod
     def from_window(cls, window):
         return cls(window, 0, 0, window.width, window.height)
 
-    def write(self, *data):
-        self.buffer.extend(array.array('H', (key for key in data)))
+    def drain(self):
+        self.buffer = array.array('H')
 
-    def writeln(self, *args):
-        self.write(*args, '\n')
+    def write(self, string):
+        self.buffer.extend(array.array('H', string if hasattr(string, '__iter__') else (string,)))
+
+    def writestr(self, string):
+        self.write((ord(c) for c in string))
+
+    def writeln(self, string):
+        self.writestr(string)
+        self.buffer.append(NEWLINE)
 
     def update(self):
         self.screen.fill(self.bg)
 
         y = 0
+
         buffer = self.buffer.tobytes()[:-1].split(b'\x0d')
 
-        for line in buffer:
+        if len(buffer) > (self.height // self.size):
+            slice = buffer[len(buffer) - (self.height // self.size):]
+        else:
+            slice = buffer
+
+        for line in slice:
             self.text(list(b for b in line if b), 0, y * self.size)
             y += 1
 
-        pygame.display.update(self.screen.get_rect())
+        return self.screen.get_rect()
