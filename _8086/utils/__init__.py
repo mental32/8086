36df6fa5210ee0e952215401cad201310ab61570
mental <m3nta1@yahoo.com>
2018-12-25 19:02:46 +0000

    Add a savefile abstraction

--- _8086/utils/__init__.py
+++ _8086/utils/__init__.py
@@ -2,9 +2,9 @@ import sys
 
 assert '_8086' in sys.modules, 'Disallowed: Cannot directly import 8086 submodules.'
 
-# Imports
 from .textbox import Textbox
 from .filesystem import Filesystem
+from .savefile import SaveFile
 
 def __getattribute__(name):
     raise AttributeError('Module attribute access disallowed.')
---
+++ _8086/utils/savefile.py
@@ -0,0 +1,12 @@
+import sys
+import os
+
+class SaveFile:
+	def __init__(self):
+		pass
+
+	def read(self):
+		pass
+
+	def write(self):
+		pass
