896e6a362eecf2ad7652ca1b43e9ff149755e340
mental <m3nta1@yahoo.com>
2018-12-29 02:55:42 +0000

    Removed old prelude module

--- _8086/prelude/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-assert '_8086' in sys.modules, 'Disallowed: Cannot directly import 8086 submodules.'
-
-from .title import TitleScreen
-
-def __getattribute__(name):
-    raise AttributeError('Module attribute access disallowed.')
