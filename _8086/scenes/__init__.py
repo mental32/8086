cfe8902499ca0177fd471fcfe2d245d48ec3e5e8
mental <m3nta1@yahoo.com>
2018-12-29 08:55:27 +0000

    Add crt animation splash screen

--- _8086/scenes/__init__.py
+++ _8086/scenes/__init__.py
@@ -3,6 +3,7 @@ import sys
 assert '_8086' in sys.modules, 'Disallowed: Cannot directly import 8086 submodules.'
 
 from .boot import Boot
+from .crt import crt_animation
 
 def __getattribute__(name):
     raise AttributeError('Module attribute access disallowed.')
---
+++ _8086/scenes/crt.py
@@ -0,0 +1,36 @@
+import itertools
+import random
+import time
+
+
+def crt_animation(window):
+    _w, _h = window.font.size('A')
+    partial_width = window.width // 10
+
+    threads = []
+    x = 0
+    color = itertools.cycle(((255, 0, 0), (0, 255, 0), (0, 0, 255)))
+
+    for thread in range(partial_width):
+        threads.append([x, 0, 10, 0, next(color)])
+        x += 10
+
+    window.screen.fill((0, 0, 0))
+
+    while threads:
+        # loop over the threads and advance their position.
+        _t = []
+
+        while threads:
+            thread = threads.pop()
+            x, y, w, h, c = thread
+
+            window.rect(x, y, w, h, color=c)
+            h += random.choice((10, 0))
+
+            if h < (window.height + 20):
+                _t.append([x, y, w, h, c])
+
+        window.update()
+        threads = _t
+        time.sleep(0.01)
