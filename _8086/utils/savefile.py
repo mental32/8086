017b8940d252ebd25a190050c52f119d79f50f5a
mental <m3nta1@yahoo.com>
2018-12-30 03:49:06 +0000

    Add SaveFile.__bool__ and __repr__

--- _8086/utils/savefile.py
+++ _8086/utils/savefile.py
@@ -1,9 +1,16 @@
 import sys
 import os
 
+
 class SaveFile:
-	def __init__(self):
-		pass
+	def __init__(self, fp):
+		self.fp = fp
+
+	def __repr__(self):
+		return f'<SaveFile: fp={self.fp!r}>'
+
+	def __bool__(self):
+		return bool(self.fp)
 
 	def read(self):
 		pass
