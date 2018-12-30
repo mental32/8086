cbd9835120150c51189bd4d5a48f41e6f9c8b985
mental <m3nta1@yahoo.com>
2018-12-30 06:05:44 +0000

    Separate level selector and game level

--- _8086/scenes/__init__.py
+++ _8086/scenes/__init__.py
@@ -3,7 +3,8 @@ import sys
 assert '_8086' in sys.modules, 'Disallowed: Cannot directly import 8086 submodules.'
 
 from .crt import crt_animation
-from .level import LevelSelector
+from .selector import LevelSelector
+from .level import GameLevel
 
 def __getattribute__(name):
     raise AttributeError('Module attribute access disallowed.')
