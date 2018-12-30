154720c8ba1eccb4f23907518eb8fce93b9c020f
mental <m3nta1@yahoo.com>
2018-12-30 06:04:05 +0000

    Add utils.colors and fix debug behaviour

--- _8086/utils/__init__.py
+++ _8086/utils/__init__.py
@@ -2,12 +2,15 @@ import sys
 
 assert '_8086' in sys.modules, 'Disallowed: Cannot directly import 8086 submodules.'
 
+from . import colors
 from .textbox import Textbox
 from .savefile import SaveFile
 
-def debug(string, __c=0):
-    print(f'[{__c}] {string}')
-    __c += 1
+
+_debug_counter = 0
+def debug(string):
+    print(f'[{_debug_counter}] {string}')
+    globals()['_debug_counter'] += 1
 
 def __getattribute__(name):
     raise AttributeError('Module attribute access disallowed.')
